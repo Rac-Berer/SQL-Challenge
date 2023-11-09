a182134a42f4198ebc9d33e1685bf8fead7c23cb
Rac-Berer <bererrm@gmail.com>
2023-11-08 23:55:37 -0500

    Update on analysis

--- EmployeeSQL/Analysis.sql
+++ EmployeeSQL/Analysis.sql
@@ -24,11 +24,10 @@ SELECT dept_manager.dept_no,
 departments.dept_name,
 dept_manager.emp_no,
 employees.last_name, 
-employees.first_name,
+employees.first_name
 FROM dept_manager
 INNER JOIN employees 
 ON dept_manager.emp_no = employees.emp_no
-ORDER BY emp_no
 INNER JOIN departments
 ON dept_manager.dept_no = departments.dept_no
-ORDER BY dept_no
+ORDER BY emp_no
